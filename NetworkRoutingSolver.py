866b2075dfec76702e46e6b834a2372ef9306489
JC May <62317562+MayDevelops@users.noreply.github.com>
2022-02-19 21:27:35 -0700

    Refactor
    Remove ArrayQueue class. Add documentation for helper methods. Add method functionality for queue initialization.

--- NetworkRoutingSolver.py
+++ NetworkRoutingSolver.py
@@ -19,24 +19,7 @@ def makeNode(id, prev, dist):
     return node
 
 
-class ArrayQueue:
-    nodes = []
-
-    def insert(self, node):
-        self.nodes.append(node)
-
-    def deleteMin(self):
-        nodeToReturn = self.nodes[0]
-        self.nodes.remove(nodeToReturn)
-        return nodeToReturn
-
-    def findV(self, nodeID):
-        for i in range(len(self.nodes)):
-            if self.nodes[i].id == nodeID:
-                return i
-        return None  # this is bad
-
-
+# noinspection PyMethodMayBeStatic
 class NetworkRoutingSolver:
     def __init__(self):
         pass
@@ -70,35 +53,91 @@ class NetworkRoutingSolver:
         #       ALSO, STORE THE RESULTS FOR THE SUBSEQUENT
         #       CALL TO getShortestPath(dest_index)
 
-        priorityQueue = ArrayQueue()
-        sourceNode = None
+        # Initialize the priority queue and get the source node
+        pQueue = self.initList(srcIndex)
+        lookupTable = pQueue
 
-        for vertex in range(len(self.network.nodes)):
-            if self.network.nodes[vertex].node_id == srcIndex:
-                sourceNode = makeNode(self.network.nodes[vertex].node_id, self.network.nodes[vertex].node_id, 0)
-            else:
-                node = makeNode(self.network.nodes[vertex].node_id, -1, 10000)
-                priorityQueue.insert(node)
-
-        priorityQueue.nodes.insert(0, sourceNode)
-
-        while priorityQueue.nodes:
-            u = priorityQueue.deleteMin()
+        while pQueue:
+            u, pQueue = self.deleteMin(pQueue)
             print(f"Popped: {u.id}")
             neighbors = self.network.nodes[u.id].neighbors
             print(f"Neighbors for {u.id}: {neighbors}")
             for v in neighbors:
                 # Find where v lives in the priority queue
-                vIndex = priorityQueue.findV(v.dest.node_id)
+                vIndex = self.getVisitorIndex(pQueue, v.dest.node_id)
+
+                # If v lives inside the priority queue then visit it
                 if vIndex is not None:
                     combinedDistanceOfUandV = u.dist + v.length
-                    if priorityQueue.nodes[vIndex].dist > combinedDistanceOfUandV:
+                    if pQueue[vIndex].dist > combinedDistanceOfUandV:
                         # Update the node that we're visiting
-                        priorityQueue.nodes[vIndex].dist = combinedDistanceOfUandV
-                        priorityQueue.nodes[vIndex].prev = u
+                        pQueue[vIndex].dist = combinedDistanceOfUandV
+                        pQueue[vIndex].prev = u
 
                         # Move the node we are visiting to the front of the priority queue
-                        priorityQueue.nodes.insert(0, priorityQueue.nodes.pop(vIndex))
+                        pQueue.insert(0, pQueue.pop(vIndex))
+
+            #todo
+            """
+            currently the priority queue is getting updated, but is also getting deleted at the same time.
+            All of the values are looking correct, but when the loop is finished the queue is empty.
+            Just need to be able to run deleteMin and still update the table at the same time.
+            Brain is tired so I need chips and salsa 👍🏽
+            """
+
 
         t2 = time.time()
         return t2 - t1
+
+    # Helper Methods
+    #################################################################
+
+    """
+    Initialize the priority queue and set the source node at the top of the priority queue.
+    
+    :keyword {G}: the graph of all of the existing nodes
+    :param srcIndex: the index of the source node in {G}
+    
+    :returns the initialized list.
+    """
+
+    def initList(self, srcIndex: int) -> list:
+        sourceNode = None
+        H = []
+        for vertex in range(len(self.network.nodes)):
+            if self.network.nodes[vertex].node_id == srcIndex:
+                sourceNode = makeNode(self.network.nodes[vertex].node_id, self.network.nodes[vertex].node_id, 0)
+            else:
+                node = makeNode(self.network.nodes[vertex].node_id, -1, 10000)
+                H.append(node)
+
+        H.insert(0, sourceNode)
+        return H
+
+    """
+    Gets the index of the node with nodeID from the current state of H.
+    
+    :param H: the priority queue
+    :param nodeID: the node id we are trying to find in H
+    
+    :returns the index of the nodes location in the priority queue
+    """
+
+    def getVisitorIndex(self, H: list, nodeID) -> int:
+        for i in range(len(H)):
+            if H[i].id == nodeID:
+                return i
+        return None  # this is bad
+
+    """
+    Pops the top of the stack.
+    
+    :param H: the priority queue
+    
+    :returns {nodeToReturn, H} a tuple containing the popped node and the updated priority queue
+    """
+
+    def deleteMin(self, H: list) -> {Node, list}:
+        nodeToReturn = H[0]
+        H.remove(nodeToReturn)
+        return nodeToReturn, H
